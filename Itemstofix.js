c29769857ac602e5ebfc9758aa29ce593931cf75
PolarisTLX <paul.a.rail@gmail.com>
2018-03-08 11:40:49 -0500

    Added base files required

---
+++ Itemstofix.js
@@ -0,0 +1,16 @@
+1. remove/consider display of image alt messages
+
+2. relocate old vesions of site.
+
+3. seperate CSS and JS Script from HTML to clean up HTML file.
+
+4.  pull images from images folder.
+
+5. Bootstrap navbar
+
+6. limit number of animations / add toggle to enable/disable animations
+
+7. create those other pages in the navbar tabs
+    //7.a  Contact us / how to order
+    //7.b  How does it work?
+    //7.c ...
