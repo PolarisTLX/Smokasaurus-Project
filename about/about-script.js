71a481840f2144dc333c9f3d691d0ade988be0da
PolarisTLX <paul.a.rail@gmail.com>
2018-03-19 15:31:17 -0400

    added new images and appear on hover effect

---
+++ about/about-script.js
@@ -0,0 +1,16 @@
+var main_image = document.getElementById("main-image");
+// var side_images = document.getElementById("side-images");
+var side_images = document.getElementsByClassName("side-images");
+
+
+main_image.addEventListener("mouseover", showSideImages);
+
+function showSideImages() {
+
+    // main_image.style.height = 100 + "px";
+
+    for (var i in side_images) {
+      side_images[i].style.visibility = "visible";
+    }
+
+}
